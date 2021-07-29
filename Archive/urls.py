d1822e7344b3003acfedbc84d033c7d0c6006a42
Forestmoher <fomoher@gmail.com>
2021-07-29 15:40:32 -0700

    Add details/delete/update pages

--- Archive/urls.py
+++ Archive/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.home, name='Archive_home'),
     path('create', views.create_item, name='Archive_create'),
     path('list', views.items_list, name='Archive_items_list'),
+    path('classList', views.object_class_list, name='Archive_object_classes'),
+    path('details/<int:pk>', views.item_detail, name='Archive_item_detail'),
+    path('delete/<int:pk>', views.delete_item, name='Archive_delete_item'),
+    path('update/<int:pk>', views.update_item, name='Archive_update_item'),
 ]
 
--- Archive/views.py
+++ Archive/views.py
@@ -21,3 +21,41 @@ def items_list(request):
     items = Item.objects.all()
     context = {'items': items}
     return render(request, 'Archive/Archive_items_list.html', context)
+
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    context = {'number': item.item_number,
+               'pk': item.pk,
+               'object_class': item.object_class,
+               'containment': item.containment_procedure,
+               'name': item.code_name,
+               'image': item.item_image
+               }
+    return render(request, 'Archive/Archive_item_detail.html', context)
+
+
+def object_class_list(request):
+    objects = ObjectClass.objects.all()
+    context = {'objects': objects}
+    return render(request, 'Archive/Archive_object_classes.html', context)
+
+
+def delete_item(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('Archive_items_list')
+    context = {'item': item}
+    return render(request, 'Archive/Archive_delete_item.html', context)
+
+
+def update_item(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    form = ItemForm(request.POST or None, instance=item)
+    context = {'form': form, 'item': item}
+    if form.is_valid():
+        form.save()
+        return redirect('Archive_item_detail', item.pk)
+    return render(request, 'Archive/Archive_update_item.html', context)
+
